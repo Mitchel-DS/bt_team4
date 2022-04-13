271d5b493435d712d6b3276bf149816407a5ef5c
Mitchel-DS <mitchel.staal@outlook.com>
2022-04-13 20:41:40 +0200

    removed h1

--- controllers/list.js
+++ controllers/list.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
-const list = async(req, res) => {
-	const Schema = mongoose.Schema;  
+const list = async (req, res) => {
+	const Schema = mongoose.Schema;
 	const scholen = mongoose.model('scholen', new Schema({
-	name: String,
-	locatie: String,
-	niveau: String,
-	onderwerpen: Array
+		name: String,
+		locatie: String,
+		niveau: String,
+		onderwerpen: Array
 	}), 'scholen');
 
 	const scholenList = await scholen.find().lean();
-	res.render('list', {scholen: scholenList});
+	res.render('list', { scholen: scholenList });
 }
 
 module.exports = {
